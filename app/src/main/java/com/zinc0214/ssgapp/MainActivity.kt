0ec24875d627e50824140ce85aa296716ef81f4c
zinc <zinc0214@gmail.com>
2020-09-03 22:44:31 +0900

    Initial commit

---
+++ app/src/main/java/com/zinc0214/ssgapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zinc0214.ssgapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_member)
+    }
+}
